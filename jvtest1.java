cb7bedc9190e474993c069c9859963c4679df1f4
Chalapathir1 <rchalapaty@gamil.com>
2020-08-18 04:17:48 +0000

    added java file

---
+++ jvtest1.java
@@ -0,0 +1,7 @@
+public class Test {
+	public static void main(String[] args) {
+		        System.out.println("Hello, Test");
+			      System.out.println("Test message");     
+			      	System.out.println("hi Devops ");
+				  }
+}
